f8aefae6b4950556d54881f7259f64d7ce0b87e7
Ovidiu Popoviciu <ovidiup13.tuta.io>
2017-07-15 16:23:35 +0100

    Adds function for converting any base to base 10

---
+++ src/algorithms/math/NumberSystems.java
@@ -0,0 +1,37 @@
+package algorithms.math;
+
+/**
+ * Created by ovidi on 15/07/2017.
+ */
+public class NumberSystems {
+
+    private static int toBase10(String n, int base){
+        if(base < 2 || base > 16){
+            return -1;
+        }
+
+        int value = 0;
+        for(int i = 0; i < n.length(); i++){
+            int digit = digitToValue(n.charAt(i));
+            if(digit < 0 || digit > base){
+                return -1;
+            }
+
+            int exp = n.length() - 1 - i;
+            value += digit * Math.pow(base, exp);
+        }
+
+        return value;
+    }
+
+    private static int digitToValue(char c){
+        return Character.getNumericValue(c);
+    }
+
+
+    public static void main(String[] args) {
+        String n = "0110";
+        int base = 2;
+        System.out.println(toBase10(n, base));
+    }
+}
