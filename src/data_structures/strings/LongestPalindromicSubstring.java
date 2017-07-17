005fb3b3cca0eb2799228109d73247069ff180ef
Ovidiu Popoviciu <ovidiup13.tuta.io>
2017-07-17 16:31:38 +0100

    Added brute force longest palindromic substring. Adds roman2int algorithm.

--- src/data_structures/strings/LongestPalindromicSubstring.java
+++ src/data_structures/strings/LongestPalindromicSubstring.java
@@ -3,8 +3,53 @@ package data_structures.strings;
 
 public class LongestPalindromicSubstring {
 
-    public static void main(String[] args) {
+    //O(n^3) solution
+    public String bruteForce(String a) {
 
+        if (a == null) {
+            return null;
+        }
+
+        int n = a.length();
+        if (n <= 1) {
+            return a;
+        }
+
+        // generate all substrings and keep track of max
+        int maxLength = -1;
+        String p = null;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                int length = 2 * i + j - 1;
+
+                if (length < maxLength) {
+                    continue;
+                }
+
+                String s = a.substring(i, i + j);
+
+                if (isPalindrome(s) && maxLength < s.length()) {
+                    maxLength = s.length();
+                    p = s;
+                }
+
+            }
+        }
+
+        return p;
+    }
+
+    private boolean isPalindrome(String s) {
+        int n = s.length();
+
+        for (int i = 0, j = n - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
--- src/data_structures/strings/Roman2Integer.java
+++ src/data_structures/strings/Roman2Integer.java
@@ -3,8 +3,53 @@ package data_structures.strings;
 
 public class Roman2Integer {
 
-    public static void main(String[] args) {
+    public int romanToInt(String a) {
 
+        int n = a.length();
+        if(n == 0){
+            return 0;
+        }
+
+        int value = 0;
+
+        for(int i = 0; i < n; i++){
+            int val = getValue(a.charAt(i));
+            if(i == n - 1 || val >= getValue(a.charAt(i+1))){
+                value += val;
+            } else {
+                value -= val;
+            }
+        }
+
+        return value;
+    }
+
+    private int getValue(char c){
+        switch(c){
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Roman2Integer r2i = new Roman2Integer();
+        String s = "MXLIV";
+        System.out.println(r2i.romanToInt(s)); // 1044
     }
 
 
